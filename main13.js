5cd76360cbe974a6cfc90dc3d5e1efcf9c5c0db6
Gabriela Dodson <dodsongabi@gmail.com>
2024-11-02 12:29:10 -0400

    Handle errors with .catch() and display error message

--- main13.js
+++ main13.js
@@ -1,31 +1,44 @@
 //Fetch Products from the API Using Fetch and Promises
 const productsContainer = document.getElementById('products-container');
 
+//loading
+productsContainer.innerHTML =`
+<div class="loading">
+    <h2>Loading...</h2>
+</div>
+`;
 fetch('https://www.course-api.com/javascript-store-products')
-.then(response => response.json())
+.then(response=>{
+    if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+})
 .then(products => {
     const productsHTML = products.map(product =>
         `
         <article class = "product-card">
             <div class="product-image">
         <img src ="${product.fields.image[0].url}"alt="${product.fields.name}">
-           <div class="product-info">
+           </div>
+        <div class="product-info">
         <h3 class= "product-name">${product.fields.name}</h3>
         <p class="company">${product.fields.company}</p>
-            <p class="price>$${product.fields.price.toFixed(2)}</p>
+            <p class="price">$${product.fields.price.toFixed(2)}"</p>
         
             </div>
             </article>
-        `
-        ).join('');
+        
+        `).join('');
         productsContainer.innerHTML = productsHTML;
     })
     .catch(error=>{
-        productsContainer.innerHTML=
-        `
+        productsContainer.innerHTML=`
+        
         <div class="error">
             <h2>Failed to load products</h2>
-            <p>${error.message}</p>
+            <p>try again later</p>
+            <button class="retry-btn" onclick="location.reload()">Try Again</button>
         </div>
         `;
     });
